6509c526363ae835f17f6be2ef8ab9cac1e924f1
Colin Burdine <colinburdine443@gmail.com>
2019-10-30 15:19:06 -0500

    added command line args to the solver

--- main.go
+++ main.go
@@ -4,23 +4,53 @@ package main
 import "C"
 
 import (
+	"flag"
 	"fmt"
 )
 
+var SupportedSolvers = map[string]bool{"dpll": true}
+
 func main() {
 
-	// parse formula:
-	formulaData, err := ParseCnf("data/uf50-01.cnf")
-	if err != nil {
-		panic(err)
+	solver := flag.String("solver", "dpll", "the solver to be used")
+
+	flag.Parse()
+	files := flag.Args()
+
+	if _, ok := SupportedSolvers[*solver]; !ok {
+		fmt.Printf("Solver \"%s\" not supported. Supported solvers are:\n", solver)
+		fmt.Println(SupportedSolvers)
+		return
 	}
 
-	cIntSentence := make([]C.int, len(formulaData.sentence), len(formulaData.sentence))
-	for k, v := range formulaData.sentence {
-		cIntSentence[k] = C.int(v)
+	if len(files) == 0 {
+		fmt.Println("No files given; expected a list of .cnf files as args")
+		return
 	}
 
-	// print result
-	result := C.dpll3Sat(&cIntSentence[0], C.int(len(cIntSentence)))
-	fmt.Println("Res: ", result)
+	for _, file := range files {
+		// parse formula:
+		formulaData, err := ParseCnf(file)
+		if err != nil {
+			panic(err)
+		}
+
+		cIntSentence := make([]C.int, len(formulaData.sentence), len(formulaData.sentence))
+		for k, v := range formulaData.sentence {
+			cIntSentence[k] = C.int(v)
+		}
+
+		// evaluate sentence:
+		fmt.Printf("[%s]: ", file)
+		result := C.dpll3Sat(&cIntSentence[0], C.int(len(cIntSentence)))
+
+		// print result
+		resultStatus := "SATISFIABLE"
+		if result == 0 {
+			resultStatus = "UNSATISFIABLE"
+		}
+		fmt.Println(resultStatus)
+
+	}
+
 }
